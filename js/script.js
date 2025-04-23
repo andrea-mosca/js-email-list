d37d7a6277cf737046409385eb308a0c09b5ff39
andrea-mosca <andrea.moscateli@gmail.com>
2025-04-23 16:13:27 +0200

    rising js code

--- js/script.js
+++ js/script.js
@@ -1,31 +1,32 @@
 const emailEl = document.getElementById("email-list");
-const apiEmailUrl = `https://flynn.boolean.careers/exercises/api/random/mail`
+const apiEmailUrl = `https://flynn.boolean.careers/exercises/api/random/mail`;
 
 // * FUNZIONE CHE AGGIUNGE UNA EMAIL ALLA LISTA
 const createHTMLEmailList = (email) => {
-    return `<li class="list-group-item">${email}</li>`
+  return `<li class="list-group-item">${email}</li>`;
 };
 
-// * CICLO CHE GENERA TRAMITE API  10 EMAIL E LE AGGIUNGE NELL'HTML
-for(let i = 0; i < 10; i++){
-    axios.get(apiEmailUrl)
-        .then(response => {
-            const userEmail = response.data.response;
-            emailEl.innerHTML += createHTMLEmailList(userEmail);
-        });
-};
+// *CREA FUNZIONE CHE GENERA TRAMITE API 10 EMAIL E LE AGGIUNGE NELL'HTML
+function fetchAndRenderEmails() {
+    for (let i = 0; i < 10; i++) {
+      axios.get(apiEmailUrl).then((response) => {
+          const userEmail = response.data.response;
+          emailEl.innerHTML += createHTMLEmailList(userEmail);
+        })
+    }
+  };
+  
+//* CHIAMO FUNZIONE DI GENERAZIONE EMAIL
+fetchAndRenderEmails();
 
 // * bottone che generi altre 10 email in sostituzione delle altre
 const generatEmailButton = document.getElementById("generate-email-button");
 
+generatEmailButton.addEventListener("click", function () {
+  emailEl.innerHTML = ``;
+  fetchAndRenderEmails();
+});
 
-generatEmailButton.addEventListener("click",function() {
-    emailEl.innerHTML = ``;
-    for(let i = 0; i < 10; i++){
-        axios.get(apiEmailUrl)
-            .then(response => {
-                const userEmail = response.data.response;
-                emailEl.innerHTML += createHTMLEmailList(userEmail);
-            });
-    };
-  });
+
+
+
