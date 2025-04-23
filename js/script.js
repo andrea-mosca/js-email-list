777e373a17b061266670955dab0008eba3bf5092
andrea-mosca <andrea.moscateli@gmail.com>
2025-04-23 15:25:15 +0200

    function check

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
-const apiEmailUrl = `https://flynn.boolean.careers/exercises/api/random/mail`
+const emailEl = document.getElementById("email-list");
 
+// funzione che aggiunge un'email alla lista
+const createHTMLEmailList = (email) => {
+    return `<li class="list-group-item">${email}/li>`
+};
+
+const apiEmailUrl = `https://flynn.boolean.careers/exercises/api/random/mail`
+ 
 axios.get(apiEmailUrl)
     .then(responsive => {
-        console.log(responsive.data.response);
+        const utentEmail = responsive.data.response;
+        for(let i = 0; i < 10; i++){
+            emailEl.innerHTML += createHTMLEmailList(utentEmail);
+        }
     });
